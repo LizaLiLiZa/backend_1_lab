c2f2c272e3b388a8d6ef8cab8e8dc05618a12492
LizaLiLiZa <117859604+LizaLiLiZa@users.noreply.github.com>
2024-09-09 18:16:39 +0300

    main programm

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Модуль содержит простую функцию hello_func.
+"""
+def hello_func():
+    """
+    Функция выводит строку 'Hello world' на экран.
+    """
+    print("Hello world")
+
+# Вызов функции
+hello_func()
